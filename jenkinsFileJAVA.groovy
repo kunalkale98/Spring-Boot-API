a586575e5457b82301db52f214f984944ed1b5f2
kunalkale98 <47416520+kunalkale98@users.noreply.github.com>
2021-06-26 11:49:18 +0530

    Update jenkinsFileJAVA.groovy

--- jenkinsFileJAVA.groovy
+++ jenkinsFileJAVA.groovy
@@ -14,9 +14,6 @@ pipeline {
         stage('Deploy') {
             steps {
                 sh 'rsync -r -e "ssh -i /home/ubuntu/kunal_123.pem" /var/lib/jenkins/workspace/Java_App/target/SpringBootDemoProject-0.0.1-SNAPSHOT.jar ubuntu@10.1.0.131:/home/ubuntu/'
-                sh 'rsync -r -e "ssh -i /home/ubuntu/kunal_123.pem" /var/lib/jenkins/workspace/Java_App/Java_App.service ubuntu@10.1.0.131:/home/ubuntu'
-                sh 'rsync -r -e "ssh -i /home/ubuntu/kunal_123.pem" /var/lib/jenkins/workspace/Java_App/start.sh ubuntu@10.1.0.131:/home/ubuntu'
-                sh 'ssh -i /home/ubuntu/kunal_123.pem ubuntu@10.1.0.45 "bash /home/ubuntu/start.sh"'
             }
         }
     }
